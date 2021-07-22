e4cc286a224076182f59337f1931e02b6018281c
jinseonghee <wlstjdgml0610@gmail.com>
2021-07-22 15:18:34 +0900

    comment 수정

--- GradeProject/src/school/School.java
+++ GradeProject/src/school/School.java
@@ -5,15 +5,17 @@ import java.util.ArrayList;
 public class School {
 
     private static School instance = new School();//학교라는 객체는 하나이기 때문에 singleton pattern으로 만듬
+                                                  //전역 객체변수로 사용하기 위해 static 객체변수로 생성
 
     private ArrayList<Student> studentList = new ArrayList<Student>();
     private ArrayList<Subject> subjectList = new ArrayList<Subject>();
 
-    private School() { //밖에선 이 학교를 만들 수 없게 default Constructorprivate으로 만듬
+    private School() { //밖에선 이 학교를 만들 수 없게 default Constructor를 private으로 만듬
 
     }
 
-    public static School getInstance() { //싱글톤 패턴
+    //getInstance 메서드를 통해 한번만 생성된 객체를 가져온다.
+    public static School getInstance() { //싱글톤 패턴(최초 한번만 new 연산자를 통하여 메모리에 할당한다.)
 
         if(instance == null)
             instance = new School();
